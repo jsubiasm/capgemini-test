5fa1ddc210c41963ff6fb7a884dd41d88d0329f0
jsubias <javier.subias-minguez@capgemini.com>
2026-08-05 10:24:37 +0200

    ya se puede ejecutar el mvn clean install sin errores. (Tienen que estar levantados los contenedores con 'docker compose up')

--- src/main/resources/db/migration/V1__init.sql
+++ src/main/resources/db/migration/V1__init.sql
@@ -19,6 +19,6 @@ CREATE TABLE users (
 -- Insertar datos iniciales
 INSERT INTO rooms (name) VALUES ('Sala 1'), ('Sala 2');
 
-INSERT INTO users (name, email, role, room_id) VALUES
-('Juan Pérez', 'juan@example.com', 'ADMIN', 1),
-('María López', 'maria@example.com', 'USER', 2);
+INSERT INTO users (name, email, dni, phone, role, room_id) VALUES
+('Juan Pérez', 'juan@example.com', '111', '111', 'superadmin', 1),
+('María López', 'maria@example.com', '222', '222', 'admin', 2);
